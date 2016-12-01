ef051299e1f8eae8b1f49be5ddd49d766e12e62d
Kenneth Chen <kchen@KennethChen.local>
2016-11-30 19:31:28 -0800

    First Commit

---
+++ gulpfile.js
@@ -0,0 +1,21 @@
+var gulp = require('gulp'),
+    resize = require('gulp-image-resize'),
+    rename = require('gulp-rename');
+
+gulp.task('resize', function(){
+    gulp.src('img-src/*.jpg')
+        .pipe(resize({
+            width: 300,
+            height: 300,
+            upscale:false
+        }))
+        .pipe(gulp.dest('img-dst/'))
+});
+
+gulp.task('rename', function(){
+    gulp.src('img-src/*.jpg')
+        .pipe(rename())
+        .pipe(gulp.dest('js/app.min.js'));
+});
+
+gulp.task('default', ['resize', 'rename']);
