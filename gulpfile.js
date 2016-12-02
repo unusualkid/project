90ce310f99c4155fb99d94b87c16238c9cd61910
Kenneth Chen <kchen@kennethchen.attlocal.net>
2016-12-01 23:09:20 -0800

    Add Gulp. Improve performance.

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,37 @@
 var gulp = require('gulp'),
     resize = require('gulp-image-resize'),
-    rename = require('gulp-rename');
+    styles = require('gulp-clean-css'),
+    rename = require('gulp-rename'),
+    clean = require('gulp-clean');
 
 gulp.task('resize', function(){
-    gulp.src('img-src/*.jpg','')
+    gulp.src(['img-src/*.{jpg,png}', '!img-src/center-image.jpg'])
         .pipe(resize({
-            width: 300,
-            height: 300,
+            width: 600,
+            height: 600,
             upscale: false
         }))
-        .pipe(gulp.dest('img-dst/'))
+        .pipe(rename(function (path) { path.basename += "-md-600"; }))
+        .pipe(gulp.dest('img-dst/'));
 });
 
-gulp.task('rename', function(){
-    gulp.src('img-src/*.jpg')
-        .pipe(rename())
-        .pipe(gulp.dest('js/app.min.js'));
+gulp.task('styles', function(){
+    gulp.src(['css-src/*.css','!css-src/*.min.css'])
+        .pipe(styles())
+        .pipe(rename(function (path) { path.basename += ".min"; }))
+        .pipe(gulp.dest('css-src/'));
 });
 
-gulp.task('default', ['resize', 'rename']);
+gulp.task('clean', function() {
+    gulp.src('img-dst/', { read: false })
+        .pipe(clean());
+    gulp.src('css-src/*min.css', { read: false })
+        .pipe(clean());
+});
+
+gulp.task('watch', function() {
+    gulp.watch('img-src/*.{jpg,png}',['resize']);
+    gulp.watch('css-src/*.css', ['styles']);
+});
+
+gulp.task('default', ['resize', 'styles', 'watch', 'clean']);
